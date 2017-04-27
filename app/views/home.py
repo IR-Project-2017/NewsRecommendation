b31816023f3774c1fe506c1326fda6188a05193d
山田　潤 <junyamada107@gmail.com>
2017-04-27 17:03:02 +0200

    add input box and get params from a request

--- app/views/home.py
+++ app/views/home.py
@@ -1,6 +1,13 @@
-from flask import Blueprint, Flask, render_template
+from flask import Blueprint, Flask, render_template, request
 app = Blueprint("home", __name__, url_prefix="")
 
 @app.route("/")
 def index():
     return render_template('home/index.html')
+
+@app.route("/search")
+def search():
+    if request.method == "GET":
+        query = request.args.get('q')
+        print query
+    return render_template('home/search.html')
