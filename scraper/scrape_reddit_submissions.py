f3f8e130d9a14d0bf1651b599bd9fc0b02d7054f
David Svantesson <david@stelapoint.com>
2017-04-27 19:46:43 +0200

    Moved try/except, should now properly catch errors and continue

--- scraper/scrape_reddit_submissions.py
+++ scraper/scrape_reddit_submissions.py
@@ -11,7 +11,7 @@ post_file = open(post_file_path, 'a')
 # List of all subreddits we want to scrape
 subreddits = ["worldnews"]
 # Define date range to crawl, Year-Month-Day
-datestart = datetime.date(2017, 4, 26)
+datestart = datetime.date(2017, 3, 26)
 dateend = datetime.date(2017, 4, 27)
 
 # Specify reddit bot, needs to be created at https://www.reddit.com/prefs/apps/
@@ -30,28 +30,28 @@ def daterange(start_date, end_date):
 for subreddit in subreddits:
     # Iterate over specified date range
     for date in daterange(datestart, dateend):
+
+        print("Started scraping date: " + date.strftime("%Y-%m-%d"))
+
         # Convert dates to Unix time
         udatestart = time.mktime(datestart.timetuple())
         udateend = time.mktime(dateend.timetuple())
 
-        submissions = None
-        # Get submissions from specified date. Just wait if server is overloaded or we get blocked.
-        while True:
-            try:
-                submissions = reddit.subreddit(subreddit).submissions(udatestart, udateend)
-            except Exception:
-                print("HTTP error, waiting 5 seconds")
-                time.sleep(5)
-                continue
-            break
+        # Specify submissions from specified date
+        submissions = reddit.subreddit(subreddit).submissions(udatestart, udateend)
 
-        # Write all submissions to file, Format is "title \t url \t doc_id \t date \n"
-        for submission in submissions:
-            title = submission.title.encode('ascii', "ignore").decode("ascii")
-            url = submission.url.encode('ascii', "ignore").decode("ascii")
-            doc_id = submission.id.encode('ascii', "ignore").decode("ascii")
-            post_date = date.strftime("%Y-%m-%d")
-            post_file.write(title + "\t" + url + "\t" + doc_id + "\t" + post_date + "\t" + subreddit + "\n")
+        # Get and Write all submissions to file, Format is "title \t url \t doc_id \t date \n", Wait 5 seconds if any server errors.
+        try:
+            for submission in submissions:
+                title = submission.title.encode('ascii', "ignore").decode("ascii")
+                url = submission.url.encode('ascii', "ignore").decode("ascii")
+                doc_id = submission.id.encode('ascii', "ignore").decode("ascii")
+                post_date = date.strftime("%Y-%m-%d")
+                post_file.write(title + "\t" + url + "\t" + doc_id + "\t" + post_date + "\t" + subreddit + "\n")
+        except Exception:
+            print("HTTP error, waiting 5 seconds")
+            time.sleep(5)
+            continue
 
         print("Done scraping date: " + date.strftime("%Y-%m-%d"))
 
