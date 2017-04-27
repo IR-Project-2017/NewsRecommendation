4bb78a2087289bdd45fc8735f5a2418b2b531d68
David Svantesson <david@stelapoint.com>
2017-04-27 18:55:11 +0200

    Rewrote scraper, can do over date ranges and different subreddits, separated into scraping and importing scripts

---
+++ scraper/import_submissions.py
@@ -0,0 +1,33 @@
+# Run this file after scraping to import into Elastic Search
+
+from elasticsearch import Elasticsearch
+
+# Open Elastic Search connection
+es = Elasticsearch()
+
+file_path = "post_file.txt"
+# Open file to import from
+import_file = open(file_path, "r")
+
+counter = 0
+
+for line in import_file.readlines():
+
+    # Make sure we skip last newline
+    if line == "\n":
+        continue
+
+    split_line = line.split("\t")
+
+    doc = {
+        "url": split_line[0],
+        "title": split_line[1],
+        'post_date': split_line[3].rstrip("\n")
+    }
+    res = es.index(index="testindex", doc_type=split_line[4].rstrip("\n"), id=split_line[2], body=doc)
+    counter += 1
+    if counter % 1000 == 0:
+
+        print("Done indexing " + str(counter) + " documents...")
+
+print("\nIndexed a total of: " + str(counter) + " documents\n")
---
+++ scraper/scrape_reddit_submissions.py
@@ -0,0 +1,51 @@
+# Run this file first to scrape speficied subreddits
+
+import praw
+import datetime
+import time
+
+# File to save to
+post_file_path = "post_file.txt"
+post_file = open(post_file_path, 'a')
+
+# List of all subreddits we want to scrape
+subreddits = ["worldnews"]
+# Define date range to crawl, Year-Month-Day
+datestart = datetime.date(2017, 4, 26)
+dateend = datetime.date(2017, 4, 27)
+
+# Specify reddit bot, needs to be created at https://www.reddit.com/prefs/apps/
+reddit = praw.Reddit(client_id='kiEkGfkxQ71Dpg',
+                     client_secret='2wk6Gihtvtx3B8PyPJ8CLV9X23s',
+                     user_agent='PrawScraper',
+                     username='schoolstuff01',
+                     password='kth123456')
+
+
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + datetime.timedelta(n)
+
+# Iterate over each subreddit
+for subreddit in subreddits:
+    # Iterate over specified date range
+    for date in daterange(datestart, dateend):
+        # Convert dates to Unix time
+        udatestart = time.mktime(datestart.timetuple())
+        udateend = time.mktime(dateend.timetuple())
+
+        # Get 1000 submissions from specified date
+        submissions = reddit.subreddit(subreddit).submissions(udatestart, udateend)
+
+        # Write all submissions to file, Format is "title \n url \n doc_id \n date \n"
+        for submission in submissions:
+            title = submission.title.encode('ascii', "ignore").decode("ascii")
+            url = submission.url.encode('ascii', "ignore").decode("ascii")
+            doc_id = submission.id.encode('ascii', "ignore").decode("ascii")
+            post_date = date.strftime("%Y-%m-%d")
+            post_file.write(title + "\t" + url + "\t" + doc_id + "\t" + post_date + "\t" + subreddit + "\n")
+
+        print("Done scraping date: " + date.strftime("%Y-%m-%d"))
+
+
+post_file.close()
