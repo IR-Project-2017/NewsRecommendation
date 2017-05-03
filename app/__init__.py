1436dc1b6292eb306cf3b573a512b535fd33dffb
山田　潤 <junyamada107@gmail.com>
2017-05-03 13:37:33 +0200

    add mongodb settings

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,12 @@ import sys, os
 sys.path.append("./app/")
 from flask import Flask, request
 from views import home
-
+from flask_mongoalchemy import MongoAlchemy
 
 app = Flask(__name__)
 app.register_blueprint(home.app)
-
+app.config['MONGOALCHEMY_DATABASE'] = 'library'
+db = MongoAlchemy(app)
 
 
 
