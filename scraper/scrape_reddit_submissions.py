9bcfa592f093cf75e918f5b23cbe09c9011034ed
David Svantesson <david@stelapoint.com>
2017-05-03 14:34:41 +0200

    Added aged groups and more subreddits

--- scraper/scrape_reddit_submissions.py
+++ scraper/scrape_reddit_submissions.py
@@ -3,16 +3,21 @@
 import praw
 import datetime
 import time
+import random
 
 # File to save to
 post_file_path = "post_file.txt"
 post_file = open(post_file_path, 'a')
 
 # List of all subreddits we want to scrape
-subreddits = ["worldnews"]
+subreddits = ["worldnews", "China", "Africa", "Australia", "unitedKingdom", "UpliftingNews", "Economics", "soccer", "baseball"]
+age_groups = ["10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70-79", "80+"]
 # Define date range to crawl, Year-Month-Day
-datestart = datetime.date(2017, 3, 26)
-dateend = datetime.date(2017, 4, 27)
+datestart = datetime.date(2017, 4, 20)
+dateend = datetime.date(2017, 5, 3)
+
+ages = {group: random.randint(0, 100) for group in age_groups}
+print(",".join(['%s:%s' % (key, value) for (key, value) in ages.items()]))
 
 # Specify reddit bot, needs to be created at https://www.reddit.com/prefs/apps/
 reddit = praw.Reddit(client_id='kiEkGfkxQ71Dpg',
@@ -39,15 +44,25 @@ for subreddit in subreddits:
 
         # Specify submissions from specified date
         submissions = reddit.subreddit(subreddit).submissions(udatestart, udateend)
-
+        scraped_subs = 0
         # Get and Write all submissions to file, Format is "title \t url \t doc_id \t date \n", Wait 5 seconds if any server errors.
         try:
             for submission in submissions:
+                # If submission is a "self-post", ignore it.
+                if submission.is_self:
+                    continue
+                # Stop scraping from this date if we have already done 300
+                if scraped_subs > 300:
+                    break
                 title = submission.title.encode('ascii', "ignore").decode("ascii")
                 url = submission.url.encode('ascii', "ignore").decode("ascii")
                 doc_id = submission.id.encode('ascii', "ignore").decode("ascii")
                 post_date = date.strftime("%Y-%m-%d")
-                post_file.write(title + "\t" + url + "\t" + doc_id + "\t" + post_date + "\t" + subreddit + "\n")
+                post_file.write(title + "\t" + url + "\t" + doc_id + "\t" + post_date + "\t")
+                ages = {group: random.randint(0, 100) for group in age_groups}
+                post_file.write(",".join(['%s:%s' % (key, value) for (key, value) in ages.items()]))
+                post_file.write("\t" + subreddit + "\n")
+                scraped_subs += 1
         except Exception:
             print("HTTP error, waiting 5 seconds")
             time.sleep(5)
