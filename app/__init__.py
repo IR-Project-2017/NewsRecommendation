a9cc1ed3a950b86682ba9fa545bbc8396139d052
山田　潤 <junyamada107@gmail.com>
2017-05-04 15:52:48 +0200

    delete flask_mongoalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ import sys, os
 sys.path.append("./app/")
 from flask import Flask, request
 from views import home
-from flask_mongoalchemy import MongoAlchemy
 
 app = Flask(__name__)
 app.register_blueprint(home.app)
